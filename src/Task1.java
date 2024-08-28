4fabb30e2f6554b4d832f3dadb91db281bb0be46
MinYoungChoe <mchoe@myseneca.ca>
2024-08-28 14:12:54 -0400

    JAC444

---
+++ src/Task1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Enter a credit card number as a long integer: \n");
+        Scanner userInput = new Scanner(System.in);
+
+        long testNum = 1;
+        System.out.println("size: " +getSize(testNum));
+
+    }
+public static int getSize(long d){
+        return Long.toString(d).length();
+}
+
+
+
+}
+
